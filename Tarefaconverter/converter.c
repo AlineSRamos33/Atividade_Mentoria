9319cfe80094e4dda1efe3a29b5b2e632d681258
Aline Ramos <alineramos@ifba.edu.br>
2024-12-17 09:59:30 -0300

    Primeira versao do programa

---
+++ Tarefaconverter/converter.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int comprimento(int);
+int massa(int);
+int volume(int);
+int temperatura(int);
+int velocidade(int);
+int potencia(int);
+int area(int);
+int tempo(int);
+int armazenamento(int);
+
+
+int main(){
+
+system("chcp 65001>nul");
+printf("Escolha a conversão desejada: ");
+
+}
